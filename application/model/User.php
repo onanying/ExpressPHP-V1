875e426eca850a732c93b04f4efe498129be8674
Jian Liu <onanying@126.com>
2017-03-14 15:24:28 +0800

    重新放入

---
+++ application/model/User.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * 模型 Sample
+ * @author 刘健 <59208859@qq.com>
+ */
+
+namespace Tiny\Model;
+
+use Tiny\Common\Model;
+
+class User extends Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getUserInfo()
+    {
+    	return array('name'=>'xiaohua', 'sex'=>'0', 'height'=>'168');
+    }
+
+}
---
+++ application/view/sample.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+	
+	Welcome ,
+	Name: <?php echo $info['name'] ?>
+	Sex: <?php echo $info['sex'] ?>
+	Height: <?php echo $info['height'] ?>
+
+</body>
+</html>
