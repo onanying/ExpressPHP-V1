fe178761fda32e14e0f1a21239ff87b9bae4c589
Jianliu <onanying@126.com>
2017-04-17 10:59:35 +0800

    增加 XML 类

--- application/webpage/controller/News.php
+++ application/webpage/controller/News.php
@@ -8,7 +8,7 @@
 namespace app\webpage\controller;
 
 use app\model\Users;
-use sys\response\View;
+use sys\response\Xml;
 
 class News
 {
@@ -36,15 +36,17 @@ class News
 
         // return View::create()->fetch('webpage.view.news.article')->assign('name', 'xiaohua')->assign('sex', 'w');
         // return View::create('webpage.view.news.article', ['name' => 'xiaohua', 'sex' => 'w']);
-        $view = new View();
-        $view->fetch('webpage.view.news.article');
-        $view->assign('name', 'xiaohua');
-        $view->assign('sex', 'w');
-        return $view;
+        // $view = new View();
+        // $view->fetch('webpage.view.news.article');
+        // $view->assign('name', 'xiaohua');
+        // $view->assign('sex', 'w');
+        // return $view;
 
         // return Json::create(['errcode' => 0, 'errmsg' => 'ok']);
 
         // return ['errCode' => 0, 'errMsg' => 'ok', 'data' => $data];
+
+        return Xml::create(['errCode' => 0, 'errMsg' => 'ok', 'data' => [['name' => 'xiaohua', 'sex' => 'w'], ['name' => 'xiaohua', 'sex' => 'w']]]);
     }
 
 }
